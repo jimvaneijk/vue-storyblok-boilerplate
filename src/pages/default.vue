f1b6011d5202b9051f8cb1fdb83572eb45eb9ffa
Jim van Eijk <jim@23g.nl>
2024-12-27 16:19:17 +0100

    fix: add missing line break in default.vue for improved readability

--- src/pages/default.vue
+++ src/pages/default.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useStoryblok } from '@storyblok/vue';
 import { useRoute } from 'vue-router';
+
 const route = useRoute();
 
 const story = await useStoryblok(route.params.slug, { version: 'draft' });
